0c3bac0027213fcda8a805c47653a8426b9b8b87
Sergi2912 <sergipalliser@gmail.com>
2023-10-24 14:00:27 +0200

    Ex11

---
+++ Ex11.py
@@ -0,0 +1,19 @@
+def menu principal()=
+    print(" " "
+      Menu principal
+        1.Calculadora enters
+        2.Calculadora reals
+        3.Sortir
+        " " ")
+     x=input("Elegeixi una opció "))
+    if x>0 and x<4:
+      return X
+    else: return 0
+
+#Programa principal
+a = menu_pricipal()
+print(a)
+
+
+
+
